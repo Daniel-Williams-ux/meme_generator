5072954b06635b748f41b04544a8388c706d5645
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-17 10:10:20 +0000

    styled header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,9 +3,9 @@ import React from "react"
 export default function Header () {
   return (
     <header className="header">
-      <img src="./images/troll-face.jpg" />
-    <h2>Header Component</h2>
-      <h4>React Course - Project 3</h4>
+      <img src="./images/troll-face.jpg" className="header--image" />
+    <h2 className="header--title">Header Component</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
       </header>
   )
 }

47da59100de9bbece61bac32ed13add43864b012
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-17 09:58:56 +0000

    created a header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from "./Components/Header"
 
 function App() {
   return (
     <main>
-      React⚛️ + Vite⚡ + Replit🌀
+     <Header />
     </main>
   );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header () {
+  return (
+    <header className="header">
+      <img src="./images/troll-face.jpg" />
+    <h2>Header Component</h2>
+      <h4>React Course - Project 3</h4>
+      </header>
+  )
+}
